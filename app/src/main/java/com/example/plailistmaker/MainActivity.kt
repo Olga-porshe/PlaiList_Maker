dc497c63eea032bd57e47271526052052cbb5d7d
olga-porshe <loli1977@list.ru>
2026-06-07 18:40:46 +0300

    init

--- app/src/main/java/com/example/plailistmaker/MainActivity.kt
+++ app/src/main/java/com/example/plailistmaker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.plailistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,29 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnMediaLibrary = findViewById<ImageButton>(R.id.media_library)
+        val btnSearch = findViewById<ImageButton>(R.id.btnSearch)
+        val btnSetting = findViewById<ImageButton>(R.id.setting)
+
+
+        btnSearch.setOnClickListener {
+            val btnSearch = Intent(this, SearchActivity::class.java)
+            startActivity(btnSearch)
         }
+
+        btnMediaLibrary.setOnClickListener {
+            val btnMediaLibrary = Intent(this, MediaLibraryActivity::class.java)
+            startActivity(btnMediaLibrary)
+        }
+
+        btnSetting.setOnClickListener {
+            val btnSetting = Intent(this, SettingsActivity::class.java)
+            startActivity(btnSetting)
+        }
+
+
     }
 }
