521690661aee9a42f5c91354e91e2568c037eec8
olga-porshe <loli1977@list.ru>
2026-06-11 17:15:10 +0300

    master

--- app/src/main/java/com/example/plailistmaker/MainActivity.kt
+++ app/src/main/java/com/example/plailistmaker/MainActivity.kt
@@ -12,7 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
+
 
         val btnMediaLibrary = findViewById<ImageButton>(R.id.media_library)
         val btnSearch = findViewById<ImageButton>(R.id.btnSearch)
--- app/src/main/java/com/example/plailistmaker/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/plailistmaker/MediaLibraryActivity.kt
@@ -9,7 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 class MediaLibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContentView(R.layout.activity_media_library)
 
     }
